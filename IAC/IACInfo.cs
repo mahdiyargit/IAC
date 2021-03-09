cdb24e92dc7430ec5710957c0637de6cc0b158d7
Mahdiyar Esmailbeigi <mahdiyar@outlook.com>
2021-03-09 11:57:28 +0330

    initial commit

--- IAC/IACInfo.cs
+++ IAC/IACInfo.cs
@@ -8,7 +8,7 @@ namespace IAC
     {
         public override string Name => "IAC";
         public override Bitmap Icon => Properties.Resources.icon24X24;
-        public override string Description => "This plugin has been written by the participants of the workshop \"Scripting & plugin development\".";
+        public override string Description => "This plugin has been written by the participants of the \"Scripting & plugin development\" workshop in Iranian Architecture Center.";
         public override Guid Id => new Guid("cd54ee94-048a-43f1-b492-150024d0640c");
         public override string AuthorName => "IranianArchitectureCenter";
         public override string AuthorContact => "info@Mahdiyar.io";
